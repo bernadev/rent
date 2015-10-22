e6ecea2ea79f03de634a04689882367478a5c89b
Diego Michel Rubio Ramirez <diego.rubio@agilekoding.com>
2015-10-22 09:13:58 -0500

    Fixes WORK_COORDINATES var

--- fetch.rb
+++ fetch.rb
@@ -8,9 +8,8 @@ OptionParser.new do |opts|
   opts.banner = "Usage: fetch.rb -c [configFile]"
   opts.on('-c', '--config CONFIGFILE', 'Configuration File'){|v| configFile = v}
 end.parse!
-configFile = "config" if configFile == ""
+configFile = "config.rb" if configFile == ""
 require "./#{configFile}"
-binding.pry
 
 loginResponseJSON= `curl -c myCookie 'http://www.compartodepa.com.mx/Account/Login' -H 'Pragma: no-cache' -H 'X-NewRelic-ID: VwAFUl9aGwcAU1NbBwM=' -H 'Origin: http://www.compartodepa.com.mx' -H 'Accept-Encoding: gzip, deflate' -H 'Accept-Language: en-US,en;q=0.8' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36' -H 'Content-Type: application/json; charset=UTF-8' -H 'Accept: application/json, text/javascript, */*; q=0.01' -H 'Cache-Control: no-cache' -H 'X-Requested-With: XMLHttpRequest' -H 'Cookie: ASP.NET_SessionId=mrnrtz5zzdr55oqgtohje0rm; __utmt=1; __utmt_global=1; __utma=266890268.11386725.1445460360.1445460360.1445473498.2; __utmb=266890268.2.10.1445473498; __utmc=266890268; __utmz=266890268.1445460360.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _dc_gtm_UA-51362008-1=1; _ga=GA1.3.11386725.1445460360' -H 'Connection: keep-alive' -H 'Referer: http://www.compartodepa.com.mx/' --data-binary '{\"UserName\":\"#{USERNAME}\",\"Password\":\"#{PASSWORD}\",\"LoginSuccessful\":false,\"RememberMe\":true}' --compressed`
 loginResponse = JSON.parse(loginResponseJSON)
@@ -33,7 +32,7 @@ urls.each do |url|
   longitude = coordinates[/^(.*?)longitude: \"(.*?)\".\n/, 2]
   room_coordinates=[latitude, longitude]
   
-  request = "curl 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{work_coordinates.join(",")}&destinations=#{room_coordinates.join(",")}&mode=walking&transit_routing_preference=less_walking&key=#{API_KEY}'"
+  request = "curl 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{WORK_COORDINATES.join(",")}&destinations=#{room_coordinates.join(",")}&mode=walking&transit_routing_preference=less_walking&key=#{API_KEY}'"
   result = `#{request}`
   unless (JSON.parse(result)["rows"][0]["elements"][0]["duration"]["value"] rescue nil).nil?
     minutes = JSON.parse(result)["rows"][0]["elements"][0]["duration"]["value"].to_f/60
