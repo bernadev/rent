c1babe76eed3993b0846f3dbc5daf7126ac3f686
Diego Michel Rubio Ramirez <diegomichel@gmail.com>
2015-11-01 14:31:39 -0600

    Gets extra information from website status, requirements and description

--- fetch.rb
+++ fetch.rb
@@ -2,6 +2,7 @@ require 'json'
 require 'pry'
 require 'optparse'
 require 'yaml'
+require 'nokogiri'
 
 configFile = ""
 OptionParser.new do |opts|
@@ -27,16 +28,27 @@ urls.each do |url|
   room = `curl -c myCookie 'http://www.compartodepa.com.mx#{url}' -H 'Pragma: no-cache' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Referer: http://www.compartodepa.com.mx/results-room/loc/52/rmax/4000/bed/1/pic/0/doub/0/furn/0/shor/0/amin/18/amax/99/pag/11/srt/3' -H 'Cookie: ASP.NET_SessionId=mrnrtz5zzdr55oqgtohje0rm; showProfileTeaserL15062218282926=false; accountcode=L15062218282926; _ga=GA1.3.11386725.1445460360; search_locations=52|53,54,55,56,62,57,58,59,60,61; haslogin=perm; usercode=2007040319551652174; checkcode=AAEAC8B5-C533-41D9-8F79-B3DD7E4DF74A; __utma=266890268.11386725.1445460360.1445460360.1445473498.2; __utmb=266890268.40.8.1445477796850; __utmc=266890268; __utmz=266890268.1445460360.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none)' -H 'Connection: keep-alive' -H 'Cache-Control: no-cache' --compressed`
   newURL = room[room.index("moved to <a href=\"/")+19, room.index("\">here</a>.")-room.index("moved to <a href=\"/")-19]
   realRoom = `curl -c myCookie 'http://www.compartodepa.com.mx/#{newURL}' -H 'Pragma: no-cache' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: en-US,en;q=0.8' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Cache-Control: no-cache' -H 'Cookie: ASP.NET_SessionId=mrnrtz5zzdr55oqgtohje0rm; showProfileTeaserL15062218282926=false; accountcode=L15062218282926; search_locations=52|53,54,55,56,62,57,58,59,60,61; haslogin=perm; usercode=2007040319551652174; checkcode=AAEAC8B5-C533-41D9-8F79-B3DD7E4DF74A; __utmt=1; __utmt_global=1; __utma=266890268.11386725.1445460360.1445460360.1445473498.2; __utmb=266890268.44.8.1445477796850; __utmc=266890268; __utmz=266890268.1445460360.1.1.utmcsr=(direct)|utmccn=(direct)|utmcmd=(none); _ga=GA1.3.11386725.1445460360; _dc_gtm_UA-51362008-1=1' -H 'Connection: keep-alive' --compressed`
+  begin
+    pricetmp = realRoom[realRoom.index("product:price:amount"), 100]
+    price = pricetmp[/^(.*?)content=\"(.*?)\" \/>\n/, 2]
+  rescue
+    price = -1
+  end
   coordinates = realRoom[realRoom.index("latitude"), 100]
   latitude= coordinates[/^latitude: \"(.*?)\".\n/, 1]
   longitude = coordinates[/^(.*?)longitude: \"(.*?)\".\n/, 2]
   room_coordinates=[latitude, longitude]
-  
   request = "curl 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=#{WORK_COORDINATES.join(",")}&destinations=#{room_coordinates.join(",")}&mode=walking&transit_routing_preference=less_walking&key=#{API_KEY}'"
   result = `#{request}`
   unless (JSON.parse(result)["rows"][0]["elements"][0]["duration"]["value"] rescue nil).nil?
+    binding.pry
+    @room_html = Nokogiri::HTML(realRoom)
+    status = @room_html.xpath("/html/body/div[3]/"+
+                              "div/div[1]/section/div[2]/p")[0].text
+    requirements = @room_html.xpath("/html/body/div[3]/div/div[1]/section/div[3]/p")[0].text
+    description = @room_html.xpath("/html/body/div[3]/div/div[1]/section/div[4]/p")[0].text
     minutes = JSON.parse(result)["rows"][0]["elements"][0]["duration"]["value"].to_f/60
-    resultTable << ["http://www.compartodepa.com.mx/#{newURL}", "%i:%i" % [minutes,(minutes-minutes.to_i)*60] ,minutes]
+    resultTable << ["http://www.compartodepa.com.mx/#{newURL}", "%i:%i" % [minutes,(minutes-minutes.to_i)*60] ,minutes, price]
   end
 end
 
